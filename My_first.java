e8d73be3cd5b0c4a4994b4e81c0719fc8cc96efb
Xuiq <czechjakub6@gmail.com>
2016-09-25 11:52:09 +0200

    Add files via upload

---
+++ My_first.java
@@ -0,0 +1,13 @@
+package wersja1;
+
+public class My_first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*		jafskfskfksdfksd
+	 * 
+	 */
+		System.out.println("My ygygv first");
+	}
+
+}
